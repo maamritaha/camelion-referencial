0ea184e102e44b853e9aecb38987a616c7cd029c
Hedi Ben-Ahmed <hedi.benahmed@vo2-group.com>
2019-06-09 22:16:53 +0200

    [CHORE] Init project.

---
+++ src/main/java/org/camelion/referential/ReferentialApplication.java
@@ -0,0 +1,13 @@
+package org.camelion.referential;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReferentialApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReferentialApplication.class, args);
+	}
+
+}
